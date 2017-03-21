09ceca1ddba7a7743044a3a47f9e64709ad18cd9
luis1018 <ceo.luisrodriguez@gmail.com>
2017-03-21 15:14:30 -0600

    procfile

--- headlines.py
+++ headlines.py
@@ -23,6 +23,6 @@ def get_news(publication="bbc"):
 	</html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
 	
 if __name__ == '__main__':
-	 app.debug = True
-     port = int(os.environ.get("PORT", 5000))
-     app.run(host='0.0.0.0', port=port)
+	app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
