52652d8ea041626588bb8d6abbaffd11aaf9067d
luis1018 <ceo.luisrodriguez@gmail.com>
2017-03-21 15:06:43 -0600

    procfile

--- headlines.py
+++ headlines.py
@@ -22,3 +22,5 @@ def get_news(publication="bbc"):
 		</body>
 	</html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
 	
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=5000, debug=True)
